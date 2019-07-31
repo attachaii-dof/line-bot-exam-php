ed31ca4794144bfe2f525007679b9c9bf995b926
attachaii-dof <53505281+attachaii-dof@users.noreply.github.com>
2019-07-31 13:21:23 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -3,7 +3,7 @@
 
 $access_token = 'jWz6HSrFBg7+KDwK6cO+mYjAwiY+lz0x+Gvdz/rNyilbvnlTUuOQmMXJX1+5K6O9nun1p2okrBgiKptyjqa7XSk1A7dDd8JV4X9Y4uZ5Ia4+YSLRiuFELwLOnbr8YG6RPsKGl9noszt1c24pEIeNEgdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+$userId = 'U11ff30ef7b8fd40bcd3731c86c16ecbd';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
